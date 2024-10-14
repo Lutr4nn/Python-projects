d27f32d88399b6f0a3e536fc5edbee0d49b8a99f
Lutr4nn <145874531+Lutr4nn@users.noreply.github.com>
2024-10-14 23:07:40 +0200

    Add files via upload

---
+++ Projet Calculatrice.py
@@ -0,0 +1,8 @@
+def main():
+    number1 = input("Enter your first number")
+    number2 = input("Enter your second number")
+    opération = input("What operation do you want to do?" ['+','-','/','*'])
+
+result = 0
+
+if 
